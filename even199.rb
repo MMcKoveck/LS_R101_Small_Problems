5734d428828055a1058d893c7cebb10083d10a74
Michael McKoveck <mckoveck@gmail.com>
2023-07-13 07:50:25 -0500

    Same as odd199

--- even199.rb
+++ even199.rb
@@ -3,12 +3,18 @@ range = (1..99)
 range.each_entry do |x|
   puts x if x.even?
 end
+
 #=begin THEIR CODE
+#Our solution is just a minor variation on the solution to print odd numbers. 
+#This time, though, we increment the value by 1 with every iteration, 
+#and only print it if it is even.
+
 value = 1
 while value <= 99
   puts value if value.even?
   value += 1
 end
+
 #=begin MY FES
 1.upto(99) {|x| puts x if x.even?}
 
