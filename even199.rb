85f2ed5a2be941f14191e0325069ab2800702cd5
Michael McKoveck <mckoveck@gmail.com>
2023-07-13 07:49:05 -0500

    Same as odd199

---
+++ even199.rb
@@ -0,0 +1,17 @@
+# This IS THE SAME AS odd199 but even.
+range = (1..99)  
+range.each_entry do |x|
+  puts x if x.even?
+end
+#=begin THEIR CODE
+value = 1
+while value <= 99
+  puts value if value.even?
+  value += 1
+end
+#=begin MY FES
+1.upto(99) {|x| puts x if x.even?}
+
+a = []                                 #Create Array
+1.upto(99) {|i| a << i}                #Fill Array with 1..99
+a.select {|x| puts x if x % 2 == 0}    #Array#select
