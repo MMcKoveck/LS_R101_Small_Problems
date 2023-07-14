212c3f515b51c945226c0d1ea0b19a339e4c6931
Michael McKoveck <mckoveck@gmail.com>
2023-07-14 09:14:22 -0500

    Works

---
+++ arithmetic.rb
@@ -0,0 +1,34 @@
+=begin
+Write a program that prompts the user for two positive integers, 
+and then prints the results of the following operations on those two numbers: 
+addition, subtraction, product, quotient, remainder, and power. 
+Do not worry about validating the input.
+
+Example
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 141050039560662968926103
+
+=begin PEDAC
+get 2 inputs
+puts all arithmetic equations 
+=end
+#=begin MY CODE
+
+puts "==> Enter the first number:"
+first = gets.chomp.to_i
+puts "==> Enter the second number:"
+second = gets.chomp.to_i
+puts "==> #{first} + #{second} = #{first + second}"
+puts "==> #{first} - #{second} = #{first - second}"
+puts "==> #{first} * #{second} = #{first * second}"
+puts "==> #{first} / #{second} = #{first / second}"
+puts "==> #{first} % #{second} = #{first % second}"
+puts "==> #{first} ** #{second} = #{first ** second}"
