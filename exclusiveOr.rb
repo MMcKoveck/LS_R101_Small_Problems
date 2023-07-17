4fe6c4f5f40c86d5fec5635928bec6e86d4ba76e
Michael McKoveck <mckoveck@gmail.com>
2023-07-17 12:22:34 -0500

    Works, no FE Explore ^ !!!

--- exclusiveOr.rb
+++ exclusiveOr.rb
@@ -33,6 +33,7 @@ elsif (true && true)
 else true
 
 =end # PEDAC
+#=begin MY CODE
 
 def xor?(first, second)
   if !first && !second
@@ -42,4 +43,26 @@ def xor?(first, second)
   else true
   end
 end
+
+#=end # MY CODE
+#=begin THEIR CODE
+
+def xor?(value1, value2)
+  return true if value1 && !value2
+  return true if value2 && !value1
+  false
+end
+
+methods whose name ends in a ? 
+should usually only return the explicit true and false values.
+
+A shorter version of this method is:
+
+def xor?(value1, value2)
+  !!((value1 && !value2) || (value2 && !value1))
+end
+
+This is more idiomatic ruby than the first solution, 
+but it does take a little more effort to figure out what is going on.
+Note that this solution requires !! to force the return value to a boolean.
 
