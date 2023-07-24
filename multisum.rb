8cc3268df7d24349d90d590929e90ae8f6e59209
Michael McKoveck <mckoveck@gmail.com>
2023-07-24 08:19:00 -0500

    Revisit for FE

--- multisum.rb
+++ multisum.rb
@@ -26,7 +26,63 @@ def multisum(num)
   arr = []
   sum = 0
   (1..num).each {|x| arr << x}
-  arr.each {|x| (sum = x + sum) if (x % 3 == 0 || x % 5 == 0) }
+  arr.each {|x| (sum += x) if (x % 3 == 0 || x % 5 == 0) }
   sum
 end
 #=end # MY CODE
+=begin THEIR CODE
+def multiple?(number, divisor)
+  number % divisor == 0
+end
+
+def multisum(max_value)
+  sum = 0
+  1.upto(max_value) do |number|
+    if multiple?(number, 3) || multiple?(number, 5)
+      sum += number
+    end
+  end
+  sum
+end
+
+Our solution begins with a multiple? method that returns true if the given number 
+is an exact multiple of divisor, false if it's not. 
+This method isn't necessary, but it makes the multisum a bit more readable.
+
+multisum does nothing fancy; it rushes headlong into the problem, 
+and comes out the other end with the correct result. 
+It adds each value that is a multiple of 3 or 5 in the range to the sum variable.
+=end # THEIR CODE
+#=begin FE
+Investigate Enumerable.inject (also known as Enumerable.reduce), 
+How might this method be useful in solving this problem? 
+(Note that Enumerable methods are available when working with Arrays.) 
+Try writing such a solution. Which is clearer? Which is more succinct?
+
+# ORIGINAL CODE TO WORK FROM
+def multisum(num) 
+  arr = []
+  sum = 0
+  (1..num).each {|x| arr << x}
+  arr.each {|x| (sum += x) if (x % 3 == 0 || x % 5 == 0) }
+  sum
+end
+
+=begin BRANDON COREY'S CODE
+def multisum(num)
+  (3..num).reduce(0) do |acc, val|
+    acc += val if val % 3 == 0 || val % 5 == 0
+    acc
+  end
+end
+=end # BRANDON COREY'S CODE
+=begin CLARISSA RODER'S CODE
+def multisum(num)
+  (1..num).to_a.select { |n| n % 3 == 0 || n % 5 == 0 }.sum
+end
+=end # CLARISSA RODER'S CODE
+=begin CAMERON ADAMS' CODE
+def multisum(num)
+  (1..num).to_a.select { |i| i % 3 == 0 || i % 5 == 0 }.inject(:+)
+end
+=end # CAMERON ADAMS' CODE
