19137b7c3c5baf239ae2bf911ee1217bbfc7b7a8
Michael McKoveck <mckoveck@gmail.com>
2023-07-31 10:43:13 -0500

    Works, Refactored to put a_m in b_m, NO FE

--- after_midnight.rb
+++ after_midnight.rb
@@ -45,3 +45,60 @@ def before_midnight(time)
   total == 1440 ? total = 0 : total
 end
 =end # MY CODE
+=begin THEIR CODE
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+def after_midnight(time_str)
+  hours, minutes = time_str.split(':').map(&:to_i)
+  (hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY
+end
+
+def before_midnight(time_str)
+  delta_minutes = MINUTES_PER_DAY - after_midnight(time_str)
+  delta_minutes = 0 if delta_minutes == MINUTES_PER_DAY
+  delta_minutes
+end
+
+As with the previous problem, we start with some useful constant definitions.
+
+The after_midnight method is simple enough; 
+we simply have to split the time strings into hours and minutes, 
+then calculate the result in minutes.
+
+You might be puzzled by .map(&:to_i); it's' a shorthand way of doing this:
+
+something.map { |string| string.to_i }
+
+In the last statement of after_midnight, we need to use % MINUTES_PER_DAY to handle a simple edge case.
+In the given time format, 00:00 and 24:00 are the same. 
+However, the way we implemented after_midnight calculates these as different times. 
+We use the modulo operator '%' and MINUTES_PER_DAY 
+to divide the calculated minutes by the number of minutes in a day, 
+then return the remainder, which will be 0 for both 00:00 and 24:00.
+
+before_midnight reuses after_midnight by simply subtracting the return value of after_midnight 
+from MINUTES_PER_DAY. This does have the unfortunate effect of setting delta_minutes to 1440 
+when we want 0, so the statement after simply fixes that.
+=end # THEIR CODE
+=begin FE
+How would these methods change if you were allowed to use the Date and Time classes?
+
+REWRITE USING Date AND Time..
+
+=end # FE
+#=begin MY CODE refactored to use am in bm
+def after_midnight(time)
+  hours = time[0, 2].to_i
+  hours == 24 ? hours = 0 : hours *= 60
+  minutes = time[3, 2].to_i
+  total = hours + minutes
+end
+
+def before_midnight(time)
+  base = after_midnight(time)
+  total = 1440 - base
+  total == 1440 ? total = 0 : total
+end
+=end # MY CODE
