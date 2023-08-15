a67c81efdd29703e6f883c3db7ff0302e1dd95d5
Michael McKoveck <mckoveck@gmail.com>
2023-08-15 06:44:39 -0500

    Works with FE and STEVEN DE JONGH'S CODE

--- digit_list.rb
+++ digit_list.rb
@@ -23,11 +23,60 @@ end
 
 =end # MY CODE
 #=begin THEIR CODE
+Brute force:
+def digit_list(number)
+  digits = []
+  loop do
+    number, remainder = number.divmod(10)
+    digits.unshift(remainder)
+    break if number == 0
+  end
+  digits
+end
+
+Idiomatic Ruby:
+(THIS IS BETTER THAN MINE:)
+
+def digit_list(number)
+  number.to_s.chars.map(&:to_i)
+end
+
+The brute force approach (which is not a derogatory term) 
+chops off digits from the end of the number one at a time, adding them to an array, 
+then returns the array. 
+The tricky part of this loop is the use of Integer.divmod 
+which divides number by 10, and returns two values: the result of performing an integer division, 
+and the remainder. For example, 12345.divmod(10) returns [1234, 5], 
+and has the advantage of being easy to understand.
+
+The second solution uses an idiomatic approach. Though somewhat dense, 
+it's' typical of the code you'll' encounter (and write!) in Ruby all the time. 
+Let's' break it down.
+
+Our goal is to convert a number to a list of its digits. 
+First, we convert the number to a string, then split it into an array of numeric characters. 
+This array is almost what we want, but we need an array of numbers, not an array of strings. 
+Enumerable.map comes to the rescue. You might find the (&:to_i) part weird, 
+but this is Ruby syntactic sugar; it's' shorthand for:
+
+something.map { |char| char.to_i }
+
+WHICH IS WHAT I DID!
 
 =end # THEIR CODE
 #=begin FE
-
+NO FE. MAYBE EXPLORE chars
 =end # FE
-
-
+#=begin STEVEN DE JONGH'S' CODE
+def digit_list(num)
+  num.digits.reverse
+end
+=end STEVEN DE JONGH'S' CODE
+#=begin WITHOUT THE REVERSE
+irb(main):1390:1* def d_l(num)
+irb(main):1391:1*   num.digits # TO CHECK .digits 
+irb(main):1392:0> end
+=> :d_l
+irb(main):1393:0> d_l(12345)
+=> [5, 4, 3, 2, 1] #HENCE THE CHAINED .reverse 
 =end
