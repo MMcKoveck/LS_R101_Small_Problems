510d72cf036a98ac458b18ca2dbb730fa88d09a1
Michael McKoveck <mckoveck@gmail.com>
2023-08-16 15:29:49 -0500

    Works, Refactored w/format FE is like 24hr problem

--- dms.rb
+++ dms.rb
@@ -52,7 +52,7 @@ def dms(num)
 
   "#{deg.to_s + DEGREE + min.floor.to_s + '\'' + sec.floor.to_s + '\"'}"
 end  
-
+# THIS WORKS
 def dms(num)
   deg = num.floor
   min = (num - deg) * 60 
@@ -66,8 +66,109 @@ def dms(num)
   string << "\""
   string
 end
+# IRB CHECK
+irb(main):1595:0> dms(0)
+=> "0°00'00\""
+irb(main):1596:0> dms(360)
+=> "360°00'00\""
+irb(main):1597:0> dms(30)
+=> "30°00'00\""
+irb(main):1598:0> dms(76.73)
+=> "76°43'48\""
+irb(main):1599:0> dms(254.6)
+=> "254°35'59\"" # THIS IS OFF BY 1"
+irb(main):1600:0> dms(93.034773)
+=> "93°02'05\""
+irb(main):1601:0> dms(0)
+=> "0°00'00\""
+irb(main):1602:0> dms(360)
+=> "360°00'00\""
+irb(main):1603:0> dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+=> true
+irb(main):1604:0> dms(254.6) == %(254°36'00")
+=> false  # THIS IS OFF BY 1"
+
+# REFACTORED WITH format
+# THIS IS THE BEST ONE
+def dms(num)
+  deg = num.floor
+  min = (num - deg) * 60 
+  sec = (min - min.floor) * 60 
+  format(%(#{deg}#{DEGREE}%02d'%02d"), min, sec)
+end
+
+# USE THIS :
+# format(%(#{deg}#{DEGREE}%02d'%02d"), min, sec)
 
 =end # MY CODE
+#=begin THEIR CODE
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
+def dms(degrees_float)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+end
+
+# IRB divmod AND format CHECK
+irb(main):1555:0> num
+=> 93.034773
+irb(main):1556:0> total_seconds = (num * 60).round
+=> 5582
+irb(main):1557:0> total_seconds.divmod(3600)
+=> [1, 1982]
+irb(main):1559:0> total_seconds
+=> 5582
+irb(main):1560:0> degrees, remaining_seconds = total_seconds.divmod(3600)
+=> [1, 1982]
+irb(main):1561:0> degrees
+=> 1
+irb(main):1562:0> remaining_seconds
+=> 1982
+irb(main):1563:0> minutes, seconds = remaining_seconds.divmod(60)
+=> [33, 2]
+irb(main):1564:0> minutes
+=> 33
+irb(main):1565:0> seconds
+=> 2
+irb(main):1566:0> degrees
+=> 1
+irb(main):1567:0> format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+=> "1°33'02\""
+
+We start by defining some useful constants - including the supplied DEGREE constant. 
+These constants aren't' essential, but they help with understanding what is going on in each step.
+
+The method starts out by converting the number of degrees to seconds; this makes it easier to 
+compute the whole number of degrees, minutes, and seconds. 
+Next, we use divmod to get the whole number of degrees, 
+and a remainder that represents the number of seconds in the fractional part of the original value. 
+We then use divmod again to split the remainder into a whole number of minutes, 
+and a whole number of seconds. The final step puts everything together with Kernel.format.
+
+It's' worth noting that the desired output contains both single and double quote characters. 
+Ruby provides a variety of ways to deal with quotes inside strings; 
+the easiest uses %(), %q() or %Q() to delimit the string. We use %() here.
+
+#=end # THEIR CODE
+#=begin FE
+Our solution returns the following results for inputs outside the range 0-360:
+dms(400) == %(400°00'00")
+dms(-40) == %(-40°00'00")
+dms(-420) == %(-420°00'00")
+
+Since degrees are normally restricted to the range 0-360, 
+can you modify the code so it returns a value in the appropriate range when 
+the input is less than 0 or greater than 360?
+
+dms(400) == %(40°00'00")
+dms(-40) == %(320°00'00")
+dms(-420) == %(300°00'00")
+
+THIS WOULD BE SIMILR TO THE 24HR PROBLEM
 
 =end
