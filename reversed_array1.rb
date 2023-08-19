cbdbd2c373fa956c20607dd7916b2b2b7c455739
Michael McKoveck <mckoveck@gmail.com>
2023-08-19 11:45:07 -0500

    Works-check user submission & parallel assignment

--- reversed_array1.rb
+++ reversed_array1.rb
@@ -1,4 +1,4 @@
-#=begin
+=begin
 Write a method that takes an Array as an argument, and reverses its elements in place; 
 that is, mutate the Array passed into this method. The return value should be the same Array object.
 
@@ -34,7 +34,7 @@ def reverse!(list)
   loop pop the array to a new Array?
   assign the new array to the original
   (same object id?)
-
+=end # PEDAC
 #=begin MY CODE
 def reverse!(list)
  reversed = []
@@ -55,6 +55,45 @@ def reverse!(list)
   end
   # THIS IS DUMB, IT JUST MAKES THE SAME ARRAY AGAIN 
 =end
+#=begin THEIR CODE
+def reverse!(array)
+  left_index = 0
+  right_index = -1
+
+  while left_index < array.size / 2
+    array[left_index], array[right_index] = array[right_index], array[left_index]
+    left_index += 1
+    right_index -= 1
+  end
+
+  array
+end
+=end # THEIR CODE
+
+This solution is straightforward; it sets one index to point to the beginning of the array, 
+another index to point to the end of the array, and then walks through the Array 
+exchanging elements at each step. Since we update the Array in place, 
+we name the method reverse! instead of reverse.
+
+Note that the loop terminates at the middle of the list. If we continued, 
+we would end up rebuilding the original array.
+
+The most interesting part of this code is the parallel assignment on line 6; 
+this is a Ruby idiom for exchanging two values without requiring an intermediate variable. 
+It's' a handy idiom to remember.
+
+In practice, of course, you would probably use Array.reverse! instead of writing this method. 
+It helps, though, to write your own versions of standard methods so you can understand how they work,
+ and how to use those principles in your own code.
+
+Note that our solution here has both a side effect (in mutating the array passed in to the method) 
+and a meaningful return value (it returns the mutated array). 
+Generally, this would be seen as bad practice 
+(you want your methods to have either a side effect, or a meaningful return value).
+
+No FE
+Some interesting user submissions though.
+
 
 
 
