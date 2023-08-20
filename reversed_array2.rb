d3255efd93cab4a1ddfaca4ad467af3ca36257fe
Michael McKoveck <mckoveck@gmail.com>
2023-08-20 07:33:51 -0500

    Works No FE

--- reversed_array2.rb
+++ reversed_array2.rb
@@ -41,3 +41,25 @@ def reverse(array)
   rev_array
 end
 =end # MY CODE
+#=begin THEIR CODE
+def reverse(array)
+  result_array = []
+  array.reverse_each { |element| result_array << element }
+  result_array
+end
+
+Well, nobody said reverse_each wasn't' allowed! 
+We could have solved this in a manner similar to the previous exercise, 
+but where would the fun be in that? reverse_each is like each, 
+except it processes the elements in reverse order. 
+Note that unlike each, reverse_each only applies to Arrays.
+
+Note in particular that our tests include list.object_id != new_list.object_id. 
+This verifies that the returned array is a brand new array, 
+and not a modified form of the original way. 
+Likewise, the second test of list == [1, 3, 2] confirms that the original array hasn't' changed.
+=end # THEIR CODE
+#=begin FE
+An even shorter solution is possible by using either .reduce or .each_with_object. 
+Just for fun, read about these methods in the Enumerable module documentation, 
+and try using one in your .reverse method.
