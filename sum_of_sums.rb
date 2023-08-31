112f69deaac6fa6ce82195bbab25f5c58323f5c0
Michael McKoveck <mckoveck@gmail.com>
2023-08-31 12:52:02 -0500

    Works, No FE ERIC'S CODE

--- sum_of_sums.rb
+++ sum_of_sums.rb
@@ -70,8 +70,36 @@ Solution 2:
 def sum_of_sums(numbers)
   sum_total = 0
   1.upto(numbers.size) do |count|
-    sum_total += numbers.slice(0, count).reduce(:+)
+    # p sum_total += numbers.slice(0, count).reduce(:+) # THIS PRINTS EACH ITERATION TO SEE THE ADDITION
+    sum_total += numbers.slice(0, count).reduce(:+) # KEEP THIS ORIGINAL LINE
   end
   sum_total
 end
 
+This problem is straightforward once you realize that you have to keep track of two different values. 
+One is a running total that we'll' call accumulator. It progressively sums up the elements of the array. 
+The other is the total sum and it should be the value returned by our method. 
+How do we compute this sum_total? We need to iterate over the array: 
+on each iteration we update the accumulator and add it to the sum_total. 
+This is what we do in our first solution.
+
+Our second solution takes a different approach. 
+Once again, we start by initializing a variable for the total sum. 
+Then we decide to sum up subsets of our numbers array and 
+add each of those sums to the sum_total variable.
+
+1.upto(numbers.size) is used to generate the correct size for each subset of our numbers array. 
+We use number.slice(0, count) to get that subset, 
+and then reduce(:+) is used on the current subset to add up all its elements. 
+Once we have computed the final sum(one that contains all numbers in our array), 
+and added that to our total sum, then we are finished.
+
+Can you see how these two solutions model the problem slightly differently? 
+Which model did you use in your own solution?
+#=begin ERIC QIAN'S' CODE
+def sum_of_sums(arr)
+  sums_arr = []
+  (0..arr.length-1).each { |idx| sums_arr << arr[0..idx].inject(:+) }
+  sums_arr.inject(:+)
+end
+=end
