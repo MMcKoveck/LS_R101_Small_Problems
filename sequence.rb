cee7ddac8ea21acc7e8d5ab5274e4af03d8fa98f
Michael McKoveck <mckoveck@gmail.com>
2023-09-07 08:42:08 -0500

    Works FE

--- sequence.rb
+++ sequence.rb
@@ -15,3 +15,18 @@ def sequence(num)
   (1..num).to_a
 end
 =end
+#=begin THEIR CODE
+def sequence(number)
+  (1..number).to_a
+end
+This simply takes advantage of Range combined with to_a which creates a range 
+from 1 up to the value of the supplied parameter number and then converts it to an array.
+=end
+#=begin FE WORKS!
+Food for thought: what do you think sequence should return if the argument is not greater than 0. 
+For instance, what should you do if the argument is -1? Can you alter your method to handle this case?
+
+def sequence(num)
+  num > 0 ? (1..num).to_a : (num..-1).to_a
+end
+=end
