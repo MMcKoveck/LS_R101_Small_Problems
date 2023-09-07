f9945717b8f2c2dbdd77ff22fcaa005b2239624f
Michael McKoveck <mckoveck@gmail.com>
2023-09-07 07:27:46 -0500

    Works No FE

--- twice.rb
+++ twice.rb
@@ -37,5 +37,30 @@ return num : num * 2
 def twice(num)
   half = num.to_s.length / 2
   return num * 2 if num.to_s.length % 2 != 0
-  num.to_s[0..(half-1)] == num.to_s[(-half)..-1] ? num : num * 2
+  num.to_s[0..(half-1)] == num.to_s[(half)..-1] ? num : num * 2
 end
+=end
+#=begin THEIR CODE
+def twice(number)
+  string_number = number.to_s
+  center = string_number.size / 2
+  left_side = center.zero? ? '' : string_number[0..center - 1]
+  right_side = string_number[center..-1]
+
+  return number if left_side == right_side
+  return number * 2
+end
+
+Once you understand that what you are looking for is the left and right side to the number, 
+all you really need to do is to derive those values and then compare them. 
+Now if the logic for getting each side seems complex it is actually probably easier to 
+break it down even further.
+
+First turn the number into a string, then find the center of the string. 
+Next, create a variable for the right and left sides. Finally compare and return based upon that.
+
+Note that there is an edge case for single digit numbers; in this case, center is calculated as 0. 
+If we don't' pay attention to this case, 
+we end up setting both left_side and right_side to string_number[0..-1], 
+which, in the case of a single character string, is equal to that character.
+=end
