ab9d1b1a16c6f73ccec4055bb51cf6ad828a8c7c
Michael McKoveck <mckoveck@gmail.com>
2023-09-07 18:43:12 -0500

    Works! FE Cody's Code

--- grocery_list.rb
+++ grocery_list.rb
@@ -34,7 +34,77 @@ end
 # THIS WORKS!!
 def buy_fruit(array) 
   list = []
-  array.map {|x| (x[1]).times{list << (x[0])} } # RETURNS [3, 1, 2] 
+  array.each {|x| (x[1]).times{list << (x[0])} } # RETURNS [3, 1, 2] NO MAP CUZ LIST NEEDS TO EXIST B4
   list # RETURNS APPROPRIATE RESULT!!
 end
 =end # MY CODE
+#=begin THEIR CODE
+# SOLUTION 1:
+def buy_fruit(list)
+  expanded_list = []
+
+  list.each do |item|
+    fruit, quantity = item[0], item[1]
+    quantity.times { expanded_list << fruit }
+  end
+
+  expanded_list
+end
+# SOLUTION 2:
+def buy_fruit(list)
+  list.map { |fruit, quantity| [fruit] * quantity }.flatten
+end
+
+In the first solution, we define a new array expanded_list and then iterate over the array 
+passed in as an argument. 
+For each subarray, we grab the quantity and the name of the fruit by using index reference 
+and push each string the required number of times into our new array. 
+We use the Integer.times method in order to execute the content of the block for the 
+number of times specified by the caller, here quantity. 
+On each iteration the block is executed and the fruit string is appended to our expanded list.
+
+Note that we could use the block parameters to access each element of the subarrays, like this:
+
+list.each do |fruit, quantity|
+  quantity.times { expanded_list << fruit }
+end
+
+The second solution is more compact. 
+We choose to use map in order to return a transformation of the array passed in as an argument. 
+Here again we use the block parameters to access each element of the subarrays. 
+We realize that we want to multiply each fruit by the given quantity, and try this:
+
+fruit * quantity => "applesapplesapples"
+This is not exactly what we want! 
+We are doing string concatenation when in fact we want an array 
+containing the required quantity of each fruit as separate strings. 
+Let's' try something else:
+
+[fruit] * quantity => ["apples", "apples", "apples"]
+
+Great! We are almost done. We now have an array containing all of our fruits in nested subarrays:
+
+[["apples", "apples", "apples"], ["orange"], ["bananas", "bananas"]]
+
+All we need is Array.flatten to reduce it down to a single large array.
+# THAT MEANS THAT THIS SHOULD WORK:
+def buy_fruit(array)
+  list = array.map {|x| [x[0]] * x[1]}.flatten 
+end
+# SO SQUARE BRACKETS SENDS ARRAY ELEMENTS, NOT CONCATENATING THE STRINGS!!!
+# DOES NOT EVEN NEED THE LIST ASSIGNATION
+def buy_fruit(array)
+  array.map {|x| [x[0]] * x[1]}.flatten
+end
+# TO REITERATE THIS IS THEIR SECOND SOLUTION
+def buy_fruit(list)
+  list.map { |fruit, quantity| [fruit] * quantity }.flatten
+end
+# SO SIMILAR!!
+# |element1, element2| 
+#=begin CODY MCKEEFRY'S' CODE
+def buy_fruit(arr)
+  arr.map { |ele| [ele.first] * ele.last }.flatten
+end
+# .first AND .last ARE AWESOME!!
+=end
